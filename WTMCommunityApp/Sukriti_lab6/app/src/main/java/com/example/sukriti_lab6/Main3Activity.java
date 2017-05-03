36eaf21501546ec3b24f5fea4de156d9f831766a
= <sukriti@sukritis-MacBook-Air.local>
2017-05-03 18:36:43 +0530

    App added

---
+++ WTMCommunityApp/Sukriti_lab6/app/src/main/java/com/example/sukriti_lab6/Main3Activity.java
@@ -0,0 +1,16 @@
+package com.example.sukriti_lab6;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("key");
+    }
+}
